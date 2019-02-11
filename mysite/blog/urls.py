deac82139bce86b6b3ff8cdf79c86cc142c5b78e
Sample <you@example.com>
2019-02-11 09:38:23 +0800

    updated feb 11 templates from database

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('test/', views.test, name='test_template'),
+    path('blog/', views.post_list, name='post_list'),
+   
 
 ]
